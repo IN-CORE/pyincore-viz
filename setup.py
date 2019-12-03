c0e55fd44e9bd7adbb60d9e948fbcd095f5248bc
vnarah2 <vnarah2@illinois.edu>
2019-12-03 15:41:52 -0600

    INCORE-1044: Fixed typosin resilience word

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=("This package is designed for visualizing the output of pyIncore analyses."
                       "pyIncore is a Python package to analyze and visualize various hazard "
                       "(earthquake, tornado, hurricane etc.) scenarios developed "
-                      "by the Center for Risk-Based Community Resilence Planning team from NCSA. "
+                      "by the Center for Risk-Based Community Resilience Planning team from NCSA. "
                       "The development is part of NIST sponsored IN-CORE (Interdependent Networked Community "
                       "Resilience Modeling Environment) initiative. "
                       "pyIncore allows users to apply hazards on infrastructure in selected areas. "
@@ -51,7 +51,7 @@ setup(
     ],
     keywords=[
         "infrastructure",
-        "resilence",
+        "resilience",
         "hazards",
         "data discovery",
         "IN-CORE",
