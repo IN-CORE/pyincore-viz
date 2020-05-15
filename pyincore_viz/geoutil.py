18ea7be290f4e78dc798ab10538a057aeae6be74
Jong Lee <jonglee1@illinois.edu>
2020-05-15 15:54:01 -0500

    removed unused imports

--- pyincore_viz/geoutil.py
+++ pyincore_viz/geoutil.py
@@ -4,8 +4,6 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-import json
-import os
 from pathlib import Path
 
 import contextily as ctx
