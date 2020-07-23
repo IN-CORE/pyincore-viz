3cf80ef2df7ae062505b5be6055fd6371b3cbe7e
Kim <ywkim@illinois.edu>
2020-07-23 10:33:50 -0500

    changed the setup.py with ipyleaflet version

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
     # TODO need to figure out what are the dependency requirements
     install_requires=[
         "branca==0.3.1",
-        "ipyleaflet==0.12.2",
-        "ipywidgets==7.5.0",
+        "ipyleaflet>=0.12.4,<0.13",
+        "ipywidgets>=7.5.0,<7.6",
         "pandas>=0.24.1",
         "geopandas>=0.6.1",
         "rasterio>=1.1.3",
