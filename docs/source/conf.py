d23cd10f59d75566a97b9599a044f0a3e3dcd1ea
Yong Wook Kim <ywkim@illinois.edu>
2021-06-11 13:18:05 -0500

    created release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ project = 'pyIncore-viz'
 author = 'Yong Wook Kim'
 
 # The short X.Y version
-version = '1.1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyincore-viz',
-    version='1.1.1',
+    version='1.2.0',
     packages=find_packages(where=".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={
