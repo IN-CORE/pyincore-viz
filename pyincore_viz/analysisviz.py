c91cfa1c474f505291e076bba44e720f9e47d30b
Chen Wang <cwang138@illinois.edu>
2021-09-14 14:53:50 -0500

    add placeholder

---
+++ pyincore_viz/analysisviz.py
@@ -0,0 +1,8 @@
+
+class AnalysisViz:
+
+    def __init__(self):
+        pass
+
+    def visualize(self):
+        print("visualize parent class!")
---
+++ pyincore_viz/model/incorehousingunitallocation.py
@@ -0,0 +1,4 @@
+# YW's way
+from pyincore_viz.analysisviz import AnalysisViz
+
+AnalysiViz.visualize(hua.get_output_dataset("result"))
