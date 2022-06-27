7eab66dbba1d1547b41106d90a1d869bf4af91c0
Jong Lee <jonglee1@users.noreply.github.com>
2022-06-27 09:37:58 -0500

    63 converting and revising readmemd to readmerst (#64)
    * added README.rst
    
    * added README.rst
    
    * fixed typo and suggestions
    
    * added reading readme file
    
    * updated version number at setup.py

--- setup.py
+++ setup.py
@@ -8,24 +8,19 @@ from setuptools import setup, find_packages
 import pkg_resources
 pkg_resources.extern.packaging.version.Version = pkg_resources.SetuptoolsLegacyVersion
 
+with open("README.rst", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
     name='pyincore-viz',
-    version='1.5.1',
+    version='1.5.2',
     packages=find_packages(where=".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={
         '': ['*.ini']
     },
     description='IN-CORE visualization python package',
-    long_description=("This package is designed for visualizing the output of pyIncore analyses."
-                      "pyIncore is a Python package to analyze and visualize various hazard "
-                      "(earthquake, tornado, hurricane etc.) scenarios developed "
-                      "by the Center for Risk-Based Community Resilience Planning team from NCSA. "
-                      "The development is part of NIST sponsored IN-CORE (Interdependent Networked Community "
-                      "Resilience Modeling Environment) initiative. "
-                      "pyIncore allows users to apply hazards on infrastructure in selected areas. "
-                      "Python framework acceses underlying data through local or remote services "
-                      "and facilitates moving and synthesizing results."),
+    long_description=readme,
     # TODO need to figure out what are the dependency requirements
     # TODO this is a hack, really should only be packages needed to run
     install_requires=[line.strip() for line in open("requirements.txt").readlines()],
