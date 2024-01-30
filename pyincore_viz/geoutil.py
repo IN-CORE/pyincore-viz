2a88bde4f21752b32fd29212ef2f22cd0f688b21
Ya-Lan Yang <ylyang@illinois.edu>
2024-01-30 16:26:26 -0600

    removed duplicated import

--- pyincore_viz/geoutil.py
+++ pyincore_viz/geoutil.py
@@ -4,7 +4,6 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-import contextily as ctx
 import geopandas as gpd
 import ipyleaflet as ipylft
 import lxml
