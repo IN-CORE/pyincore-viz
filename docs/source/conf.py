6468da0b90a9f4769b1a99ab369a42e14a4507fd
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2024-02-02 13:43:49 -0600

    Release 1.10.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ project = 'pyIncore-viz'
 author = 'Yong Wook Kim'
 
 # The short X.Y version
-version = '1.9'
+version = '1.10'
 # The full version, including alpha/beta/rc tags
-release = '1.9.0'
+release = '1.10.0'
 
 # -- General configuration ---------------------------------------------------
 
