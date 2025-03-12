401985cb48592aba4da7b8b6950f6298bc2cff56
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2025-03-12 14:35:19 -0500

    retrieve layer check option in get_wms_map

--- pyincore_viz/geoutil.py
+++ pyincore_viz/geoutil.py
@@ -525,7 +525,7 @@ class GeoUtil:
             flask_thread.start()
             print("Flask proxy server started on " + pyincore_viz_globals.LOCAL_PROXY_URL)
 
-    def get_wms_map(datasets: list, use_local_proxy=False):
+    def get_wms_map(datasets: list, use_local_proxy=False, layer_check=False):
         """
         Generates an ipyleaflet map with WMS layers for the provided datasets.
 
@@ -533,6 +533,7 @@ class GeoUtil:
             datasets (list): A list of pyincore Dataset objects.
             use_local_proxy (bool): If True, uses a local Flask proxy (for local development).
                                     If False (default), uses the direct GeoServer WMS URL.
+            layer_check (bool): boolean for checking the layer availability in wms server.
 
         Returns:
             obj: An ipyleaflet Map with WMS layers.
@@ -548,8 +549,45 @@ class GeoUtil:
         wms_layers = []
         bbox_all = [9999, 9999, -9999, -9999]
 
+        # the reason for checking this layer_check on/off is that
+        # the process could take very long time based on the number of layers in geoserver.
+        # the process could be relatively faster if there are not many layers in the geoserver
+        # but the processing time could increase based upon the increase of the layers in the server
+        # by putting on/off for this layer checking, it could make the process faster.
+        if layer_check:
+            try:
+                wms = WebMapService(wms_url + "?", version="1.1.1")
+            except lxml.etree.XMLSyntaxError:
+                # The error is caused because it failed to parse the geoserver's return xml.
+                # This error will happen in geoserver when there is not complete dataset ingested,
+                # and it is very hard to avoid due to current operation setting.
+                # It should be passed because this is a proof of the geoserver service is working,
+                # and the further layer_check related operation should be stopped
+                layer_check = False
+            except Exception:
+                raise Exception("Geoserver failed to set WMS service.")
+
         for dataset in datasets:
             wms_layer_name = "incore:" + dataset.id
+            # check availability of the wms layer
+            # TODO in here, the question is the, should this error quit whole process
+            # or just keep going and show the error message for only the layer with error
+            # if it needs to throw an error and exit the process, use following code block
+            # if layer_check:
+            #     wms[dataset.id].boundingBox
+            # else:
+            #     raise KeyError(
+            #         "Error: The layer " + str(dataset.id) + " does not exist in the wms server")
+            # if it needs to keep going with showing all the layers, use following code block
+            if layer_check:
+                try:
+                    wms[dataset.id].boundingBox
+                except KeyError:
+                    print(
+                        "Error: The layer "
+                        + str(dataset.id)
+                        + " does not exist in the wms server"
+                    )
             wms_layer = WMSLayer(
                 url=wms_url,
                 layers=wms_layer_name,
