c36d63ffe30cdfc6b7c969873786bd27d2039389
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2025-05-02 15:45:58 -0500

    Deprecated WMS related methods (#208)
    * Deprecated WMS related methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [Unreleased]
+### Changed
+- Method deprecated in geoutil for visualiing WMS layers. [#238](https://github.com/IN-CORE/pyincore-viz/issues/238)
+
+
 ## [1.11.1] - 2025-02-20
 
 ### Changed
--- pyincore_viz/geoutil.py
+++ pyincore_viz/geoutil.py
@@ -460,7 +460,7 @@ class GeoUtil:
         m.add_control(ipylft.LayersControl())
         return m
 
-    @staticmethod
+    @deprecated(version='1.12.0', reason="This class is deprecated. There will be no support for WMS layer visualization.")
     def get_wms_map(
         datasets: list,
         wms_url=pyincore_viz_globals.INCORE_GEOSERVER_WMS_URL,
@@ -539,7 +539,7 @@ class GeoUtil:
 
         return m
 
-    @staticmethod
+    @deprecated(version='1.12.0', reason="This class is deprecated. There will be no support for WMS layer visualization.")
     def get_gdf_wms_map(
         datasets, wms_datasets, wms_url=pyincore_viz_globals.INCORE_GEOSERVER_WMS_URL
     ):
